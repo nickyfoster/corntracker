c3613505a07c11e3b773c70baf718e9dccf9bc37
Nikita Gulyaev <n.gulyaev@just-ai.com>
2022-12-21 10:57:22 -0300

    Initial Commit

---
+++ Corny/CornyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornyApp.swift
+//  Corny
+//
+//  Created by Nick Foster on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CornyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
